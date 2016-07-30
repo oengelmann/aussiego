91c8396e86c1d00615563f64ad3feace33677b6c
John Trengrove <john@trengrove.nz>
2016-07-30 14:33:36 +1000

    Add new models

---
+++ models/example.js
@@ -0,0 +1,8 @@
+// Examples of adding 3d objects to OSMBuildings
+ osmb.addOBJ('mario.obj', { latitude: -33.855143, longitude: 151.209900 }, { scale: 0.3, id: "my_object_1"});
+ osmb.addOBJ('cat.obj', { latitude: -33.858143, longitude: 151.209900 }, { scale: 100.0, rotation: 45, id: "my_object_2", color: "#ffcc00"});
+ osmb.addOBJ('RB_TROUT.obj', { latitude: -33.858143, longitude: 151.212900 }, { scale: 200.0, rotation: 45, id: "my_object_2", color: "blue"});
+  osmb.addOBJ('grass.obj', { latitude: -33.855143, longitude: 151.209900 }, { scale: 50.0, rotation: 0, id: "grass1", color: "green"});
+  osmb.addOBJ('tree.obj', { latitude: -33.862148383431155, longitude: 151.2154769897461 }, { scale: 0.3, rotation: 0, id: "grass2", color: "green"});  
+  osmb.addOBJ('tree.obj', { latitude: -33.86606826093143, longitude: 151.21736526489258 }, { scale: 0.3, rotation: 0, id: "grass3", color: "green"});
+  osmb.addOBJ('grass.obj', { latitude: -33.87216153133538, longitude: 151.21140003204346 }, { scale: 50.0, rotation: 0, id: "grass4", color: "green"}); 
